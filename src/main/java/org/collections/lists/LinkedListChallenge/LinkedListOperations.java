75e3e4699428eae60f6717d3b425abaaa1a92567
TonyYuta <tonykagaya@gmail.com>
2018-04-28 10:35:09 -0700

    init

---
+++ src/main/java/org/collections/lists/LinkedListChallenge/LinkedListOperations.java
@@ -0,0 +1,25 @@
+/**
+ *   File Name: LinkedListOperations.java<br>
+ *
+ *   Yutaka<br>
+ *   Created: Apr 28, 2018
+ *   
+ */
+
+package org.collections.lists.LinkedListChallenge;
+
+
+/**
+ * LinkedListOperations //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Yutaka
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class LinkedListOperations {
+}
